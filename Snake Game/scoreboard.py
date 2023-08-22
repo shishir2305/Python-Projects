0747ccfecc6d6a92d48f9d6d11256132017b2724
Shishir Jha <shishir230501@gmail.com>
2023-08-22 21:06:25 +0530

    first commit

---
+++ Snake Game/scoreboard.py
@@ -0,0 +1,27 @@
+from turtle import Turtle
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align="center",
+                   font=("Courier", 15, "normal"))
+        self.hideturtle()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align="center",
+                   font=("Courier", 15, "normal"))
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center",
+                   font=("Courier", 15, "normal"))
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
